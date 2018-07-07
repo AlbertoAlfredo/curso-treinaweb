c62d9ee8e3b7457c98be9ef63cce834a655fee9c
AlbertoAlfredo <raioalfredo@gmail.com>
2018-07-07 14:20:00 -0300

    Inicio do curso

---
+++ HelloWorld/src/br/com/treinaweb/helloworld/principal/Variaveis.java
@@ -0,0 +1,16 @@
+package br.com.treinaweb.helloworld.principal;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nomePessoa = "Joao";
+		int idade = 35;
+		System.out.println("Hello World!!!!! Caraio");
+		System.out.println(nomePessoa);
+		System.out.println("Olá, " + nomePessoa + " e você tem " + idade + " anos.");
+		
+
+	}
+
+}
